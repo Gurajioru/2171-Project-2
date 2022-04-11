76ce62b7164d5f0f30238c8fdab66c429b355bc5
Jelani Smith <rathalosrocks@gmail.com>
2022-04-11 14:55:38 -0500

    change main

--- src/recordsUI/Home.java
+++ src/recordsUI/Home.java
@@ -66,7 +66,7 @@ public class Home extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -77,7 +77,7 @@ public class Home extends JFrame {
 				}
 			}
 		});
-	}
+	}*/
 
 	/**
 	 * Create the frame.
